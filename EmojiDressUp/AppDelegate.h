e3c397826ff8f2aee5e9e597e9afe28ac9331c50
Ben Haas <bhaas@bigaxtech.com>
2016-10-29 03:50:08 +0200

    Initial Commit

---
+++ EmojiDressUp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EmojiDressUp
+//
+//  Created by Bendt Jensen on 29/10/16.
+//  Copyright © 2016 Bendt Jensen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
